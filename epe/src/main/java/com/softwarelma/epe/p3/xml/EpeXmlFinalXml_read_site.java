53bd35a3285b043d2203ec171d732576405e0c91
softwarelma <softwarelma@pgmail.com>
2016-11-13 21:55:26 +0100

    +execute all labels before executing other sents +xml_read_site(url) +version 1.0.5 - execute all labels before - xml_read_site

--- epe/src/main/java/com/softwarelma/epe/p3/xml/EpeXmlFinalXml_read_site.java
+++ epe/src/main/java/com/softwarelma/epe/p3/xml/EpeXmlFinalXml_read_site.java
@@ -11,24 +11,15 @@ import com.softwarelma.epe.p1.app.EpeAppException;
 import com.softwarelma.epe.p2.exec.EpeExecParams;
 import com.softwarelma.epe.p2.exec.EpeExecResult;
 
-import javafx.application.Application;
-
 public final class EpeXmlFinalXml_read_site extends EpeXmlAbstract {
 
     @Override
     public EpeExecResult doFunc(EpeExecParams execParams, List<EpeExecResult> listExecResult) throws EpeAppException {
-        String postMessage = "xml_read_site, expected the URL and optionally the \"dynamic\" mode.";
+        String postMessage = "xml_read_site, expected the URL.";
         String url = this.getStringAt(listExecResult, 0, postMessage);
         this.log(execParams, "URL: " + url);
-        String dynamic = this.getStringAt(listExecResult, 1, postMessage, null);
-        String str;
-
-        if (dynamic == null) {
-            str = this.readSiteDynamic(url);
-        } else {
-            str = this.readSite(url);
-        }
-
+        String str = this.readSite(url);
+        this.log(execParams, "Content:");
         this.log(execParams, str);
         return this.createResult(str);
     }
@@ -52,13 +43,6 @@ public final class EpeXmlFinalXml_read_site extends EpeXmlAbstract {
         } catch (IOException e) {
             throw new EpeAppException("readSite url: " + url, e);
         }
-    }
-
-    private String readSiteDynamic(String url) {
-        // Application.launch(AppFx.class, null);
-        // Application.launch(WebViewSample.class, null);
-        Application.launch(AppFx2.class, null);
-        return null;
     }
 
 }
