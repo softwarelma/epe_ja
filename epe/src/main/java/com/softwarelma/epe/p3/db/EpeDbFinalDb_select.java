b918d19e32184ec766495ab3873479a0db74616a
softwarelma <softwarelma@gmail.com>
2017-06-08 15:29:17 +0200

    LIMIT +db_select

--- epe/src/main/java/com/softwarelma/epe/p3/db/EpeDbFinalDb_select.java
+++ epe/src/main/java/com/softwarelma/epe/p3/db/EpeDbFinalDb_select.java
@@ -22,12 +22,15 @@ import com.softwarelma.epe.p2.exec.EpeExecResult;
 public final class EpeDbFinalDb_select extends EpeDbAbstract {
 
     public static final String PROP_HEADER = "header";
+    public static final String PROP_LIMIT = "limit";
+    public static final int PROP_LIMIT_DEFAULT = 10;
     private static final Map<String, DataSource> mapUrlAndDataSource = new HashMap<>();
 
     @Override
     public EpeExecResult doFunc(EpeExecParams execParams, List<EpeExecResult> listExecResult) throws EpeAppException {
         String postMessage = "db_select, expected the data source and the select.";
         DataSource dataSource = this.getDataSourceAt(listExecResult, 0, postMessage);
+        String limitStr = retrievePropValueOrNull("db_select", listExecResult, PROP_LIMIT);
         String headerStr = retrievePropValueOrDefault("db_select", listExecResult, PROP_HEADER, "false");
         EpeAppUtils.checkContains(new String[] { "true", "false" }, "header", headerStr);
         boolean header = headerStr.equals("true");
@@ -36,32 +39,41 @@ public final class EpeDbFinalDb_select extends EpeDbAbstract {
         for (int i = 1; i < listExecResult.size(); i++) {
             if (!this.isPropAt(listExecResult, i, postMessage)) {
                 String select = this.getStringAt(listExecResult, i, postMessage);
-                readQuery(dataSource, select, listListStr, header);
+                readQuery(dataSource, select, limitStr, listListStr, header);
             }
         }
 
         this.log(execParams, listListStr, null);
         return this.createResult(listListStr, null);
+        // TODO limit to prop
     }
 
-    public static String addLimits(DataSource dataSource, String select) throws EpeAppException {
+    public static String addLimits(DataSource dataSource, String select, String limitStr) throws EpeAppException {
         EpeAppUtils.checkNull("dataSource", dataSource);
         EpeAppUtils.checkNull("select", select);
         String dataSourceClassName = dataSource.getClass().getName();
+        int limit;
+
+        try {
+            limit = Integer.parseInt(limitStr);
+            limit = limit < 1 || limit > 99999 ? PROP_LIMIT_DEFAULT : limit;
+        } catch (NumberFormatException e) {
+            limit = PROP_LIMIT_DEFAULT;
+        }
 
         if (dataSourceClassName.endsWith(".OracleDataSource")) {
-            return "SELECT * FROM (" + select + ") WHERE ROWNUM < 10";
+            return "SELECT * FROM (" + select + ") WHERE ROWNUM <= " + limit;
         } else {
             throw new EpeAppException("Unknown data source class name: " + dataSourceClassName);
         }
     }
 
-    public static void readQuery(DataSource dataSource, String select, List<List<String>> listListStr, boolean header)
-            throws EpeAppException {
+    public static void readQuery(DataSource dataSource, String select, String limitStr, List<List<String>> listListStr,
+            boolean header) throws EpeAppException {
         EpeAppUtils.checkNull("dataSource", dataSource);
         EpeAppUtils.checkNull("select", select);
         EpeAppUtils.checkNull("listListStr", listListStr);
-        select = addLimits(dataSource, select);
+        select = addLimits(dataSource, select, limitStr);
 
         try (Connection connection = dataSource.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(select);
@@ -98,7 +110,7 @@ public final class EpeDbFinalDb_select extends EpeDbAbstract {
 
                 for (int i = 0; i < resultSetMetaData.getColumnCount(); i++) {
                     Object o = resultSet.getObject(i + 1);
-                    listStr.add(o == null ? "" : o + "");// FIXME to str, see date, decimals, etc
+                    listStr.add(o == null ? "" : o + "");// TODO to str, see date, decimals, etc
                 }
 
                 listListStr.add(listStr);
